02ee4f412c4289e3173d13194f19c1bab7ed662f
Rafael Oliveira <faeltwister@gmail.com>
2022-12-17 11:28:40 -0300

    formulario adicionado

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 
-
 import Banner from './componentes/Banner/Banner';
-import CampoTexto from './componentes/CampoTexto/';
+import Formulario from './componentes/Formulario';
 
 function App() {
   return (
     <div className="App">
       <Banner />
-      <CampoTexto/>
-      <CampoTexto/>
-      <CampoTexto/>
+      <Formulario />
+
     </div>
   );
 }
---
+++ src/componentes/Formulario/index.js
@@ -0,0 +1,34 @@
+import CampoTexto from '../CampoTexto'
+import ListaSuspensa from '../ListaSuspensa'
+import './Formulario.css'
+
+const Formulario = ()=>{
+    const times = [
+        'Programação',
+        'Front-END',
+        'Back-END',
+        'Data-Science',
+        'Devops',
+        'UX',
+        'Mobile',
+        'Inovação & Gestão'
+    ]
+
+
+    
+    return(
+        <section className='formulario'>
+            <form>
+                <h2>Preencha os dados para criar o card do colaborador.</h2>
+                <CampoTexto label="Nome do Funcionário" placeholder="Informe o nome" />
+                <CampoTexto label="Cargo" placeholder="Informe o cargo"/>
+                <CampoTexto label="Imagem" placeholder="Informe a imagem"/>
+                <ListaSuspensa label="Time" itens={times}/>
+               
+                
+            </form>
+        </section>
+    )
+}
+
+export default Formulario
