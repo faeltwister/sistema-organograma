0753f3a07229da6ea1187e554b3f59212197b496
Rafael Oliveira <faeltwister@gmail.com>
2022-12-19 20:31:00 -0300

    formulario

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -1,13 +1,20 @@
+
 import './CampoTexto.css'
 //Função mais normal
 
 const CampoTexto = (props)=>{
+
+    
+
+    const aoDigitar = (e)=>{
+        props.aoAlterado(e.target.value)
+    }
     
     return(
         <div className="campo-texto">
             <label>{props.label}</label>
             
-            <input required={props.obrigatorio} placeholder={props.placeholder} />
+            <input value={props.valor} onChange={aoDigitar} required={props.obrigatorio} placeholder={props.placeholder} />
         </div>
     )
 
--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,7 +1,10 @@
+import { useState } from 'react'
 import Botao from '../Botao'
 import CampoTexto from '../CampoTexto'
 import ListaSuspensa from '../ListaSuspensa'
 import './Formulario.css'
+
+
 
 const Formulario = ()=>{
     const times = [
@@ -17,18 +20,46 @@ const Formulario = ()=>{
 
     const AoSalvar = (e)=>{
         e.preventDefault();
-        console.log('Form foi submetido!');
+        console.log('Form foi submetido!',nome , cargo , imagem, time);
 
     }
+
+    const [nome,setNome] = useState('');
+    const [cargo,setCargo] = useState('');
+    const [imagem,setImagem] = useState('');
+    const [time,setTime] = useState('');
     
     return(
         <section className='formulario'>
             <form onSubmit={AoSalvar}>
                 <h2>Preencha os dados para criar o card do colaborador.</h2>
-                <CampoTexto obrigatorio={true} label="Nome do Funcionário" placeholder="Informe o nome" />
-                <CampoTexto obrigatorio={true} label="Cargo" placeholder="Informe o cargo"/>
-                <CampoTexto label="Imagem" placeholder="Informe a imagem"/>
-                <ListaSuspensa label="Time" itens={times}/>   
+                <CampoTexto 
+                 valor={nome}
+                 aoAlterado={valor=>setNome(valor)}
+                 obrigatorio={true} 
+                 label="Nome do Funcionário"
+                 placeholder="Informe o nome" 
+                />
+                
+                <CampoTexto
+                 obrigatorio={true} 
+                 label="Cargo"
+                 placeholder="Informe o cargo"
+                 valor={cargo}
+                 aoAlterado={valor=>setCargo(valor)}
+                 
+                />
+
+
+                <CampoTexto 
+                 label="Imagem" 
+                 placeholder="Informe a imagem"
+                 valor={imagem}
+                 aoAlterado={valor=>setImagem(valor)}
+                />
+
+                <ListaSuspensa valor={time} aoAlterado={valor=>setTime(valor)} label="Time" itens={times}/>  
+
                 <Botao>Criar Card</Botao>             
             </form>
         </section>
